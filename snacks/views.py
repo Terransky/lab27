e2339087103338131fdb39c3bd588a815bf9f775
Anthony Turinski <71283758+Terransky@users.noreply.github.com>
2022-08-24 04:34:03 -0700

    added hrefs

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
 # Create your views here.
+
+
+class SnackList(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+    # context_object_name = 'snack_list'  # can be swapped with object_list in {% for %}
+
+
+class SnackDetail(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
